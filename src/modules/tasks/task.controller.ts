b92433daf4d272e01584445a6670dce82e44efde
agarci1994 <46927442+agarci1994@users.noreply.github.com>
2025-03-07 17:05:28 +0100

    doc: Integrate Swagger documentation for tasks API

--- src/modules/tasks/task.controller.ts
+++ src/modules/tasks/task.controller.ts
@@ -9,18 +9,59 @@ import {
 } from '@nestjs/common';
 import { TasksService } from './application/tasks.service';
 import { Types } from 'mongoose';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new task' })
+  @ApiBody({
+    description: 'Data to create a task',
+    schema: {
+      type: 'object',
+      properties: {
+        originalPath: { type: 'string', example: 'input/imagen.jpeg' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Data to create a task',
+    schema: {
+      type: 'object',
+      properties: {
+        originalPath: { type: 'string', example: 'input/imagen.jpeg' },
+        status: { type: 'string', example: 'pending' },
+        price: { type: 'number', example: 20.9 },
+        _id: { type: 'string', example: '12312ab3235235m12' },
+      },
+    },
+  })
   async createTask(@Body('originalPath') originalPath: string) {
     const task = await this.tasksService.createTask(originalPath);
     return { taskId: task._id, status: task.status, price: task.price };
   }
 
   @Get(':taskId')
+  @ApiOperation({ summary: 'Get a task by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Find task',
+    example: {
+      type: 'object',
+      properties: {
+        originalPath: { type: 'string', example: 'input/imagen.jpeg' },
+        price: { type: 'number', example: 20.9 },
+        status: { type: 'string', example: 'failed' },
+        _id: { type: 'string', example: '12312ab3235235m12' },
+        images: { type: 'array', example: [] },
+      },
+    },
+  })
+  @ApiResponse({ status: 404, description: 'Task not found.' })
+  @ApiResponse({ status: 400, description: 'Task id not valid.' })
   async getTask(@Param('taskId') taskId: string) {
     if (!Types.ObjectId.isValid(taskId)) {
       throw new HttpException('Id not valid', HttpStatus.BAD_REQUEST);
